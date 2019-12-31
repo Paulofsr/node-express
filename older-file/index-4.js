171b89fbe5e95c9eec7a6d125d205807822368eb
Paulo Francisco <paulofsr@yahoo.com.br>
2019-12-31 02:19:29 -0200

    The assignment 1

--- older-file/index-4.js
+++ older-file/index-4.js
@@ -4,18 +4,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promoRouter');
-const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
-const port = 4000;
+const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'))
 
@@ -24,6 +20,7 @@ app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.end(`<html><body><h1>This is an Express Server.</h1></body></html>`);
 });
+
 
 
 const server = http.createServer(app);
